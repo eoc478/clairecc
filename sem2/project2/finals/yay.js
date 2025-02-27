9ccc786bef39be981ab4b0fdf27dea57f80c1549
eoc478 <eoc478@newschool.edu>
2025-02-26 20:58:51 -0500

    yay!

--- sem2/project2/finals/yay.js
+++ sem2/project2/finals/yay.js
@@ -7,13 +7,13 @@ let explode = false;
 const word = "yay"
 
 function preload(){
-    myFont = loadFont("../fonts/kindergarten.ttf")
+    myFont = loadFont("../../../assets/Oliver-Regular.ttf")
 }
 
 function setup(){
-    createCanvas(600, 400).parent("prot1sketch");
+    createCanvas(600, 400).parent("yay");
     rectMode(CENTER);
-    pointArray = myFont.textToPoints(word, 15, 250, 300, {sampleFactor: .2}); //generate points for yay
+    pointArray = myFont.textToPoints(word, 15, 325, 310, {sampleFactor: .2}); //generate points for yay
 
     for (let i = 0; i < pointArray.length; i++) {
         let yPos = random(-200, -50); // Random starting height above the canvas
@@ -22,7 +22,7 @@ function setup(){
 }
 
 function draw(){
-    background("#322a52");
+    background(20,20,20 );
     noStroke();
 
     for(let i = 0; i < confettiPieces.length; i++){
@@ -32,7 +32,26 @@ function draw(){
 
 }
 
-//i am so bad at classes, confetti falls down to form "yay"
+function resetConfetti(){
+    explode = false;
+    confettiPieces = []; //had to chatGPT this, I didn't know I had to clear the array
+    for (let i = 0; i < pointArray.length; i++) {
+        let yPos = random(-200, -50);
+        confettiPieces.push(new confetti(pointArray[i].x, yPos, pointArray[i].y));
+    } //resets it
+}
+
+function keyPressed(){
+        resetConfetti();
+}
+
+function mousePressed(){
+    explode = true;
+    for (let i = 0; i < confettiPieces.length; i++) {
+        confettiPieces[i].explode(); // explode the confetti
+    }
+}
+
 class confetti{
     constructor(xPos, yPos, endY){
         this.x = xPos;
@@ -40,7 +59,10 @@ class confetti{
         this.endY = endY; //ending position
         this.length = random(5, 15);
         this.width = random(5, 10);
-        this.speed = random(2, 10);
+        this.fallingSpeed = random(2, 10);
+
+        this.xSpeed = 0; //when confetti explodes
+        this.ySpeed = 0;
 
         this.r = random(100, 255);
         this.g = random(100, 255);
@@ -48,11 +70,17 @@ class confetti{
     }
     update(){
         if (this.y < this.endY) { //if confetti is above target psition, keep falling
-            this.y += this.speed;
+            this.y += this.fallingSpeed;
             if (this.y > this.endY) {
               this.y = this.endY; 
             }
           }
+          if(explode){
+            this.x += this.xSpeed;
+            this.y += this.ySpeed;
+            this.ySpeed += 0.1; //gravity
+            //.5 gravity for fun
+        }
     }
     display(){
         push();
@@ -61,4 +89,12 @@ class confetti{
         rect(this.x, this.y, this.length, this.width);
         pop();
     }
+
+    explode(){
+        this.xSpeed = random(-50, 50); //(left, right)
+        this.ySpeed = random(-50, 50); //(up, down)
+        //50 for fun
+    }
 }
+
+
