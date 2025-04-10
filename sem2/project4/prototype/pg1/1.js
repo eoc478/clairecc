ddb78f1192e68446661dbfeda3f56c3eaa365d8b
eoc478 <eoc478@newschool.edu>
2025-04-09 22:10:41 -0400

    prot

---
+++ sem2/project4/prototype/pg1/1.js
@@ -0,0 +1,15 @@
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("value");
+let cat = document.getElementById("cat");
+let cats = ["catStressed, catMeh, catHappy"];
+output.innerHTML = slider.value; // Display the default slider value
+
+//https://www.youtube.com/watch?v=c3Dw7rdG4N4&list=PLNCevxogE3fhJDWT6urvq5lN5mdC0NiTP&index=3 for cats changing (didnt add any yet)
+
+//update
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  cat.innerHTML = [cats, slider.value];
+}
+
